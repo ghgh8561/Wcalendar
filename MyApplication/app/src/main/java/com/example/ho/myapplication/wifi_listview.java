eb0d88454e3a806d814c00e5aa533737e4c1141e
pyg100794 <pyg100794@gmail.com>
2019-01-07 14:44:24 +0900

    리스트뷰추가

--- MyApplication/app/src/main/java/com/example/ho/myapplication/wifi_listview.java
+++ MyApplication/app/src/main/java/com/example/ho/myapplication/wifi_listview.java
@@ -1,17 +1,33 @@
 package com.example.ho.myapplication;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class wifi_listview extends AppCompatActivity {
+
+    SQLiteDatabase database;
+    ArrayList<String> items = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wifi_listview);
+
+        String tableName = "tableName";
+        opendatabase();
+        lookupdata(tableName);
+        ListView listView = findViewById(R.id.listview);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(arrayAdapter);
 
         Button button = findViewById(R.id.go_insertactivity);
         button.setOnClickListener(new View.OnClickListener() {
@@ -21,5 +37,23 @@ public class wifi_listview extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void opendatabase() {
+        String databasename = "databaseName";
+        database = openOrCreateDatabase(databasename, MODE_PRIVATE, null);
+    }
+
+    public void lookupdata(String tablename) {
+        if (database != null) {
+            items.clear();
+            String sql = "Select mac FROM " + tablename;
+            Cursor cursor = database.rawQuery(sql, null);
+            for (int i = 0; i < cursor.getCount(); i++) {
+                cursor.moveToNext();
+                String name = cursor.getString(cursor.getColumnIndex("mac"));
+                items.add(name);
+            }
+        }
     }
 }
