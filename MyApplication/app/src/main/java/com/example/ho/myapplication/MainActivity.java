adac20c902dae6ee4123d7a93f1462b4ef71da49
ghgh8561 <ghgh8561@gmail.com>
2019-01-07 11:41:57 +0900

    와이파이, 디비 테스트

--- MyApplication/app/src/main/java/com/example/ho/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/ho/myapplication/MainActivity.java
@@ -1,10 +1,23 @@
 package com.example.ho.myapplication;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    SQLiteDatabase database;
+    WifiManager mng;
+    WifiInfo info;
+    String mac;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +26,159 @@ public class MainActivity extends AppCompatActivity {
 
         Intent intent = new Intent(this, MyService.class);
         startService(intent);
+
+        String databaseName = "databaseName";
+        openDatabase(databaseName);
+
+        Button button = (Button)findViewById(R.id.db_create_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String tableName = "tableName";
+                createTable(tableName);
+            }
+        });
+
+        Button button2 = (Button)findViewById(R.id.db_insert_button);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mng = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+                info = mng.getConnectionInfo();
+                mac = info.getBSSID();
+
+                insertData(mac);
+            }
+        });
+
+        Button button3 = (Button)findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (database != null) {
+                    String sql = "DROP table " + "tableName";
+                    database.execSQL(sql);
+
+                    String sql2 = "DROP table " + "memo";
+                    database.execSQL(sql2);
+
+                    Log.d("MyService", "테이블 삭제됨.");
+                } else {
+                    Log.d("MyService", "먼저 데이터베이스를 오픈하세요.");
+                }
+            }
+        });
+
+        Button button4 = (Button)findViewById(R.id.db_selectdata_button);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String tableName = "tableName";
+                selectData(tableName);
+            }
+        });
+
+        editText = (EditText)findViewById(R.id.EditText);
+        Button button5 = (Button)findViewById(R.id.db_memo_button);
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String tableName = "memo";
+                memo(tableName);
+                mng = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+                info = mng.getConnectionInfo();
+                mac = info.getBSSID();
+                String title = editText.getText().toString().trim();
+                memoInsert(mac, title);
+            }
+        });
+
+        Button button6 = findViewById(R.id.go_main2_button);
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+    private void openDatabase(String databaseName) {
+        database = openOrCreateDatabase(databaseName, MODE_PRIVATE, null);
+        if (database != null) {
+            Log.d("MyService", "데이터베이스 오픈됨.");
+        }
+    }
+
+    private void createTable(String tableName) {
+        if (database != null) {
+            String sql = "CREATE TABLE IF NOT EXISTS " + tableName + "(_id integer PRIMARY KEY autoincrement, mac text)";
+            database.execSQL(sql);
+
+            Log.d("MyService", "테이블 생성됨.");
+        } else {
+            Log.d("MyService", "먼저 데이터베이스를 오픈하세요.");
+        }
+    }
+
+    private void insertData(String mac) {
+        Log.d("MyService", "insertData() 호출됨.");
+
+        if (database != null) {
+            String sql = "insert into tableName(mac) values(?)";
+            Object[] params = {mac};
+
+            database.execSQL(sql, params);
+
+            Log.d("MyService", "데이터 추가함.");
+        } else {
+            Log.d("MyService", "먼저 데이터베이스를 오픈하세요.");
+        }
+    }
+
+    private void selectData(String tableName) {
+        Log.d("MyService", "selectData() 호출됨.");
+
+        if (database != null) {
+            String sql = "select mac from " + tableName;
+            Cursor cursor = database.rawQuery(sql, null);
+
+            for (int i = 0; i < cursor.getCount(); i++) {
+                cursor.moveToNext();
+                String mac = cursor.getString(0);
+
+                Log.d("MyService", "#" + i + "->" + mac);
+            }
+
+            cursor.close();
+        } else {
+            Log.d("MyService", "먼저 데이터베이스를 오픈하세요.");
+        }
+    }
+
+    private void memo(String tableName) {
+        if (database != null) {
+            String sql = "CREATE TABLE IF NOT EXISTS " + tableName + "(mac text, title text)";
+            database.execSQL(sql);
+
+            Log.d("MyService", "테이블 생성됨.");
+        } else {
+            Log.d("MyService", "먼저 데이터베이스를 오픈하세요.");
+        }
+    }
+
+    private void memoInsert(String mac, String title) {
+        if (database != null) {
+            String sql = "insert into memo(mac, title) values(?,?)";
+            Object[] params = {mac, title};
+
+            database.execSQL(sql, params);
+
+            Log.d("MyService", "데이터 추가함.");
+        } else {
+            Log.d("MyService", "먼저 데이터베이스를 오픈하세요.");
+        }
+    }
+
 }
--- MyApplication/app/src/main/java/com/example/ho/myapplication/MyService.java
+++ MyApplication/app/src/main/java/com/example/ho/myapplication/MyService.java
@@ -6,15 +6,17 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.media.RingtoneManager;
 import android.net.ConnectivityManager;
 import android.net.Network;
 import android.net.NetworkCapabilities;
-import android.net.NetworkInfo;
 import android.net.NetworkRequest;
 import android.net.Uri;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
@@ -22,7 +24,9 @@ import android.util.Log;
 
 public class MyService extends Service {
     ConnectivityManager cm;
-    NetworkInfo ninfo;
+    SQLiteDatabase database;
+    String mac_address;
+    String title;
     public MyService() {
     }
 
@@ -31,8 +35,6 @@ public class MyService extends Service {
 
         cm = (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
 
-        ninfo = cm.getActiveNetworkInfo();
-
         NetworkRequest request = new NetworkRequest.Builder()
                 .addCapability(NetworkCapabilities.NET_CAPABILITY_NOT_METERED)
                 .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
@@ -40,29 +42,71 @@ public class MyService extends Service {
         cm.registerNetworkCallback(request, new ConnectivityManager.NetworkCallback() {
             public void onAvailable (Network network) {
                 Log.d("MyService", "테스트 onAvailable");
-
-                notificationService();
             }
             public void onLost(Network network) {
                 Log.d("MyService", "테스트 onLost");
+
+                openDatabase("databaseName");
+
+                createTable();
+
+                selectData();
             }
         });
-
         return START_STICKY;
     }
 
-    /*private void wifiFunction() {
+    private void openDatabase(String databaseName) {
+        database = openOrCreateDatabase(databaseName, MODE_PRIVATE, null);
+
+        if (database != null) {
+            Log.d("MyService", "데이터베이스 오픈됨.");
+        }
+    }
+
+    private void createTable() {
+        if (database != null) {
+            String sql = "CREATE TABLE IF NOT EXISTS " + "tableName" + "(_id integer PRIMARY KEY autoincrement, mac text)";
+            database.execSQL(sql);
+
+            String sql2 = "CREATE TABLE IF NOT EXISTS " + "memo" + "(mac text, title text)";
+            database.execSQL(sql2);
+
+            Log.d("MyService", "테이블 생성됨.");
+        } else {
+            Log.d("MyService", "먼저 데이터베이스를 오픈하세요.");
+        }
+    }
+
+    private void selectData() {
+        Log.d("MyService", "selectData() 호출됨.");
+
         WifiManager mng = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
         WifiInfo info = mng.getConnectionInfo();
-        String mac = info.getBSSID();
-    }*/
+        mac_address = info.getBSSID();
+
+        if (database != null) {
+            String sql = "Select mac FROM " + "tableName" + " WHERE mac = '" + mac_address + "'";
+            Cursor cursor = database.rawQuery(sql, null);
+
+            if (cursor.getCount() != 0) {
+                notificationService();
+            }
+
+            cursor.close();
+        } else {
+            Log.d("MyService", "먼저 데이터베이스를 오픈하세요.");
+        }
+    }
 
     private void notificationService() {
+        memoSelect();
+
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "default");
 
         builder.setSmallIcon(R.mipmap.ic_launcher);
-        builder.setContentTitle("알림 제목");
-        builder.setContentText("알림 내용");
+        builder.setContentTitle(mac_address);
+        builder.setContentText(title);
 
         Intent intent = new Intent(this, MyService.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
@@ -90,6 +134,27 @@ public class MyService extends Service {
                     NotificationManager.IMPORTANCE_DEFAULT));
         }
         manager.notify(1, builder.build());
+    }
+
+    private void memoSelect() {
+        Log.d("MyService", "selectData() 호출됨.");
+
+        if (database != null) {
+            String sql = "SELECT mac, title FROM " + "memo" + " WHERE mac = '" + mac_address + "'";
+            Cursor cursor = database.rawQuery(sql, null);
+
+            for (int i = 0; i < cursor.getCount(); i++) {
+                cursor.moveToNext();
+                String mac = cursor.getString(0);
+                title = cursor.getString(1);
+
+                Log.d("MyService", "#" + i + "->" + mac + "->" + title);
+            }
+
+            cursor.close();
+        } else {
+            Log.d("MyService", "먼저 데이터베이스를 오픈하세요.");
+        }
     }
 
     @Override
