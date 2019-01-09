b2bff9947fc905a7c78a8d64e4c5d546a97fe701
cds5412 <cds5412@gmail.com>
2019-01-09 15:34:13 +0900

    캘린더클릭

--- MyApplication/app/src/main/java/com/example/ho/myapplication/Main2Activity.java
+++ MyApplication/app/src/main/java/com/example/ho/myapplication/Main2Activity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -15,6 +18,10 @@ public class Main2Activity extends AppCompatActivity {
 
         Button set_btn = findViewById(R.id.setting);
         Button set_2_btn = findViewById(R.id.setting_2);
+        CalendarView calendarview = findViewById(R.id.calendarview);
+        final TextView plan_list = findViewById(R.id.plan_list);
+
+
         set_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -27,6 +34,14 @@ public class Main2Activity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), testing.class);
                 startActivity(intent);
+            }
+        });
+        calendarview.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView CalendarView, int i, int i1, int i2) {
+                plan_list.setText("Date: " +i + "/" + i1 + 1 + "/" + i2);
+
+                Toast.makeText(getApplicationContext(), i1 + 1 + "월" + i2 + "일", Toast.LENGTH_SHORT).show();
             }
         });
     }
